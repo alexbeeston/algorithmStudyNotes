a5a773af516ea241507c36a28c8b448c82dc8539
Alex Beeston <Abeeston@DHICorp.com>
2024-11-14 16:37:45 -0700

    Add unit test for other sorting algorithms.

--- Tests/TestSorting.cs
+++ Tests/TestSorting.cs
@@ -42,87 +42,73 @@ public class TestSorting
     }
 
     [TestMethod]
+    public void TestSelectionSort()
+    {
+        TestSortingAlgorithm(SelectionSort.Main);
+    }
+
+    [TestMethod]
+    public void TestInsertionSort()
+    {
+        TestSortingAlgorithm(InsertionSort.Main);
+    }
+
+    [TestMethod]
     public void TestBubbleSort()
     {
         TestSortingAlgorithm(BubbleSort.Main);
     }
+
+    [TestMethod]
+    public void TestQuickSort()
+    {
+        TestSortingAlgorithm(QuickSort.Main);
+    }
 }
 
 
-    //[TestMethod]
-    //public void TestSelectionSort()
-    //{
-    //	for (int i = 0; i < sortingTests; i++)
-    //	{
-    //		int[] testCase = GetArrayOfRandomInts(sortingCapacity);
-    //		Algorithms.SelectionSort(testCase);
-    //		AssertSortedList(testCase, SortingAlgorithm.SelectionSort);
-    //	}
-    //}
 
-    //[TestMethod]
-    //public void TestInsertionSort()
-    //{
-    //	for (int i = 0; i < sortingTests; i++)
-    //	{
-    //		int[] testCase = GetArrayOfRandomInts(sortingCapacity);
-    //		Algorithms.InsertionSort(testCase);
-    //		AssertSortedList(testCase, SortingAlgorithm.SelectionSort);
-    //	}
-    //}
+//[TestMethod]
+//public void TestQuickSelect()
+//{
+//	asdfjasdfkj
+//	Random random = new();
+//	for (int i = 0; i < sortingTests; i++)
+//	{
+//		int[] testCase = [6, 9, 1, 10, 0, 20];
+//		int[] testCaseCopy = new int[testCase.Length];
+//		Array.Copy(testCase, 0, testCaseCopy, 0, testCase.Length);
+//		int k = 3;
+//		int kthLargestItem = Algorithms.QuickSelect(testCase, k);
+//		Algorithms.QuickSort(testCaseCopy);
+//		Assert.IsTrue(kthLargestItem == testCaseCopy[k - 1]);
+//	}
+//}
 
-    //[TestMethod]
-    //public void TestQuickSort()
-    //{
-    //	for (int i = 0; i < sortingTests; i++)
-    //	{
-    //		int[] testCase = GetArrayOfRandomInts(sortingCapacity);
-    //		Algorithms.QuickSort(testCase);
-    //		AssertSortedList(testCase, SortingAlgorithm.SelectionSort);
-    //	}
-    //}
+//private int[] GetArrayOfRandomInts(int size, int min = int.MinValue, int max = int.MaxValue)
+//{
+//	int[] arr = new int[size];
+//	Random random = new Random();
+//	for (int i = 0; i < size; i++)
+//	{
+//		arr[i] = random.Next(min, max);
+//	}
 
-    //[TestMethod]
-    //public void TestQuickSelect()
-    //{
-    //	asdfjasdfkj
-    //	Random random = new();
-    //	for (int i = 0; i < sortingTests; i++)
-    //	{
-    //		int[] testCase = [6, 9, 1, 10, 0, 20];
-    //		int[] testCaseCopy = new int[testCase.Length];
-    //		Array.Copy(testCase, 0, testCaseCopy, 0, testCase.Length);
-    //		int k = 3;
-    //		int kthLargestItem = Algorithms.QuickSelect(testCase, k);
-    //		Algorithms.QuickSort(testCaseCopy);
-    //		Assert.IsTrue(kthLargestItem == testCaseCopy[k - 1]);
-    //	}
-    //}
+//	return arr;
+//}
 
-    //private int[] GetArrayOfRandomInts(int size, int min = int.MinValue, int max = int.MaxValue)
-    //{
-    //	int[] arr = new int[size];
-    //	Random random = new Random();
-    //	for (int i = 0; i < size; i++)
-    //	{
-    //		arr[i] = random.Next(min, max);
-    //	}
+//private void AssertSortedList(int[] arr, SortingAlgorithm algorithm)
+//{
+//	if (arr.Length <= 1)
+//	{
+//		return;
+//	}
 
-    //	return arr;
-    //}
-
-    //private void AssertSortedList(int[] arr, SortingAlgorithm algorithm)
-    //{
-    //	if (arr.Length <= 1)
-    //	{
-    //		return;
-    //	}
-
-    //	for (int i = 1; i < arr.Length; i++)
-    //	{
-    //		Assert.IsTrue(arr[i - 1] <= arr[i], $"Failed for algorithm {algorithm}.");
-    //	}
-    //}
+//	for (int i = 1; i < arr.Length; i++)
+//	{
+//		Assert.IsTrue(arr[i - 1] <= arr[i], $"Failed for algorithm {algorithm}.");
+//	}
+//}
 
 //public enum SortingAlgorithm
 //{
