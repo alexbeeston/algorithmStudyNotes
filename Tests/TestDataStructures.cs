4ab974d3c0af232c795344bf23443253401976dc
Alex Beeston <Abeeston@DHICorp.com>
2024-11-15 08:45:31 -0700

    Enhance queue test#

--- Tests/TestDataStructures.cs
+++ Tests/TestDataStructures.cs
@@ -9,18 +9,25 @@ public class TestDataStructures
 	[TestMethod]
 	public void TestQueue()
 	{
-		foreach (int[] ints in TestUtils.UnsortedArrays)
+		foreach (int[] arr in TestUtils.UnsortedArrays)
 		{
 			Algorithms.DataStructures.Queue<int> testQueue = new Algorithms.DataStructures.Queue<int>();
 			System.Collections.Generic.Queue<int> systemQueue = new System.Collections.Generic.Queue<int>();
-			foreach (int i in ints)
+			for (int i = 0; i < arr.Length; i++)
 			{
-				testQueue.Enqueue(i);
-				systemQueue.Enqueue(i);
+				testQueue.Enqueue(arr[i]);
+				systemQueue.Enqueue(arr[i]);
 				Assert.IsTrue(testQueue.Peek() == systemQueue.Peek());
-				Assert.IsTrue(testQueue.Rear() == i);
+				Assert.IsTrue(testQueue.Rear() == arr[i]);
 				Assert.IsTrue(testQueue.Size == systemQueue.Count);
 				Assert.IsFalse(testQueue.IsEmpty);
+				if (i % 5 == 4)
+				{
+					int testDequeuedItem = testQueue.Dequeue();
+					int systemDequeuedItem = systemQueue.Dequeue();
+					Assert.IsTrue(testDequeuedItem == systemDequeuedItem);
+					Assert.IsTrue(testQueue.Size == systemQueue.Count);
+				}
 			}
 
 			while (!testQueue.IsEmpty)
