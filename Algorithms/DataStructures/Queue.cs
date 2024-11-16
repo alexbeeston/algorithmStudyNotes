ad916f3e4ae0d05bc6e94ba7860403971d416704
Alex Beeston <Abeeston@DHICorp.com>
2024-11-15 20:33:24 -0700

    move node class to utils.

--- Algorithms/DataStructures/Queue.cs
+++ Algorithms/DataStructures/Queue.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Algorithms.DataStructures;
+﻿namespace Algorithms.DataStructures;
 
 public class Queue<T>
 {
--- Algorithms/Utils.cs
+++ Algorithms/Utils.cs
@@ -2,33 +2,61 @@
 
 public class Utils
 {
-	public static void SwapItems(int[] arr, int i, int j)
-	{
+    public static void SwapItems(int[] arr, int i, int j)
+    {
         int tempValue = arr[i];
-		arr[i] = arr[j];
-		arr[j] = tempValue;
+        arr[i] = arr[j];
+        arr[j] = tempValue;
+    }
+
+    /// <summary>
+    /// Selects <paramref name="right"/> as a pivot, then moves all items between <paramref name="left"/> and <paramref name="right"/>
+    /// left or right of the pivot, then returns the pivot. Items equal to the pivot are placed right of the pivot.
+    /// </summary>
+    /// <returns>The index of the pivot.</returns>
+    public static int QuickPivot(int[] arr, int left, int right)
+    {
+        int i = left; // all items left of i are less than arr[right]
+        for (int j = left; j <= right; j++)
+        {
+            if (arr[j] < arr[right])
+            {
+                SwapItems(arr, i, j);
+                i++;
+            }
+        }
+
+        SwapItems(arr, i, right);
+        return i;
+    }
+
+    public const string EmptyStructMessage = "Data structure is empty";
+}
+
+public class EmptyDataStructureException
+{
+}
+
+/// <summary>
+/// A node with a pointer to another node.
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class Node<T>
+{
+	/// <summary>
+	/// Instantiates a node.
+	public Node(T data)
+	{
+		Data = data;
 	}
 
 	/// <summary>
-	/// Selects <paramref name="right"/> as a pivot, then moves all items between <paramref name="left"/> and <paramref name="right"/>
-	/// left or right of the pivot, then returns the pivot. Items equal to the pivot are placed right of the pivot.
+	/// The next node in the queue.
 	/// </summary>
-	/// <returns>The index of the pivot.</returns>
-	public static int QuickPivot(int[] arr, int left, int right)
-	{
-		int i = left; // all items left of i are less than arr[right]
-		for (int j = left; j <= right; j++)
-		{
-			if (arr[j] < arr[right])
-			{
-				SwapItems(arr, i, j);
-				i++;
-			}
-		}
+	public Node<T> Next { get; set; }
 
-		SwapItems(arr, i, right);
-		return i;
-	}
-
-	public const string EmptyStructMessage = "Data structure is empty";
+	/// <summary>
+	/// Data on the node.
+	/// </summary>
+	public T Data { get; set; }
 }
