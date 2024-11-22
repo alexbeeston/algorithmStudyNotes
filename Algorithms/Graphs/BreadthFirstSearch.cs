67dfe425225ad3e5f416fa9389fcf8314f201ab1
Alex Beeston <Abeeston@DHICorp.com>
2024-11-22 10:25:27 -0700

    Implemented BFS, though still need test cases and to figure out if there's a simpler way to manage vertices that are already in the queue.##

---
+++ Algorithms/Graphs/BreadthFirstSearch.cs
@@ -0,0 +1,31 @@
+﻿namespace Algorithms.Graphs;
+
+public static class BreadthFirstSearch
+{
+    public static List<int> Main(WeightedAdjacencyGraph graph, int rootVertex)
+    {
+        bool[] visited = new bool[graph.NumVertices];
+        bool[] inTheQueue = new bool[graph.NumVertices];
+        DataStructures.Queue<int> queue = new DataStructures.Queue<int>();
+        queue.Enqueue(rootVertex);
+        inTheQueue[rootVertex] = true;
+        List<int> visitedVertices = new List<int>();
+        while (!queue.IsEmpty)
+        {
+            int vertexBeingVisited = queue.Dequeue();
+            foreach (WeightedEdge edge in graph.Edges[vertexBeingVisited])
+            {
+                if (!visited[edge.Destination] && !inTheQueue[edge.Destination])
+                {
+                    queue.Enqueue(edge.Destination);
+                    inTheQueue[edge.Destination] = true;
+                }
+            }
+
+            visited[vertexBeingVisited] = true;
+            visitedVertices.Add(vertexBeingVisited);
+        }
+
+        return visitedVertices;
+    }
+}
---
+++ Tests/TestGraphs.cs
@@ -0,0 +1,18 @@
+﻿using Algorithms.Graphs;
+
+namespace Tests;
+
+[TestClass]
+public class TestGraphs
+{
+    [TestMethod]
+    public void TestBreadthFirstSearch()
+    {
+        WeightedAdjacencyGraph graph = new WeightedAdjacencyGraph(3);
+        graph.AddEdge(0, 1, 1);
+        graph.AddEdge(0, 2, 1);
+        graph.AddEdge(1, 2, 1);
+
+        List<int> ints = BreadthFirstSearch.Main(graph, 0);
+    }
+}
