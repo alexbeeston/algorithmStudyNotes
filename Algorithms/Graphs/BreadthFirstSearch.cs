698c2ae6f537e4b974d3909c671abb57e8ff82f9
Alex Beeston <alexmbeeston@gmail.com>
2024-11-22 21:42:42 -0700

    Simplify BFS

--- Algorithms/Graphs/BreadthFirstSearch.cs
+++ Algorithms/Graphs/BreadthFirstSearch.cs
@@ -5,24 +5,21 @@ public static class BreadthFirstSearch
     public static List<int> Main(WeightedAdjacencyGraph graph, int rootVertex)
     {
         bool[] visited = new bool[graph.NumVertices];
-        bool[] inTheQueue = new bool[graph.NumVertices];
         DataStructures.Queue<int> queue = new DataStructures.Queue<int>();
         queue.Enqueue(rootVertex);
-        inTheQueue[rootVertex] = true;
         List<int> visitedVertices = new List<int>();
         while (!queue.IsEmpty)
         {
             int vertexBeingVisited = queue.Dequeue();
             foreach (WeightedEdge edge in graph.Edges[vertexBeingVisited])
             {
-                if (!visited[edge.Destination] && !inTheQueue[edge.Destination])
+                if (!visited[edge.Destination])
                 {
                     queue.Enqueue(edge.Destination);
-                    inTheQueue[edge.Destination] = true;
+                    visited[vertexBeingVisited] = true;
                 }
             }
 
-            visited[vertexBeingVisited] = true;
             visitedVertices.Add(vertexBeingVisited);
         }
 
