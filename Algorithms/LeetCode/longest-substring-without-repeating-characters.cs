9852a4e5da43de2d88cfeb4afd3bad49b3f28e25
Alex Beeston <Abeeston@DHICorp.com>
2024-11-26 09:28:36 -0700

    Add LeetCode Challenges

---
+++ Algorithms/LeetCode/longest-substring-without-repeating-characters.cs
@@ -0,0 +1,33 @@
+﻿namespace Algorithms.LeetCode;
+
+public class longest_substring_without_repeating_characters
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        return LengthOfLongestSubstringStartingAt(s.ToCharArray(), 0, 0, 0);
+    }
+
+    public int LengthOfLongestSubstringStartingAt(char[] s, int lowerBound, int largestFoundYet, int initialUpperBound)
+    {
+        if (lowerBound > s.Length - 1)
+        {
+            return Math.Max(0, largestFoundYet);
+        }
+
+        int numUniqueChars = initialUpperBound - lowerBound;
+        for (int upperBound = initialUpperBound; upperBound < s.Length; upperBound++)
+        {
+            for (int i = lowerBound; i < upperBound; i++)
+            {
+                if (s[i] == s[upperBound])
+                {
+                    return LengthOfLongestSubstringStartingAt(s, i + 1, Math.Max(largestFoundYet, numUniqueChars), upperBound + 1);
+                }
+            }
+
+            numUniqueChars++;
+        }
+
+        return Math.Max(numUniqueChars, largestFoundYet);
+    }
+}
