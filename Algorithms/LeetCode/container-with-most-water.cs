846df73fe9708f8995d9ea98757f2af128a8dff0
Alex Beeston <abeeston@dhicorp.com>
2024-12-05 16:28:06 -0700

    container with most water (timed out but should give correct answer, gives correct answer for trivial cases)

---
+++ Algorithms/LeetCode/container-with-most-water.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+
+public class container_with_most_water
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        for (int i = 0; i < height.Length; i++)
+        {
+            for (int j = i + 1; j < height.Length; j++)
+            {
+                int candidateArea = Math.Min(height[i], height[j]) * (j - i);
+                maxArea = Math.Max(maxArea, candidateArea);
+            }
+        }
+
+        return maxArea;
+    }
+}
