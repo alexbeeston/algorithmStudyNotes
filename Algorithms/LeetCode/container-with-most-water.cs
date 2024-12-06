72cbadf4de36f5c719e7f04ddda35baac43c2b61
Alex Beeston <abeeston@dhicorp.com>
2024-12-06 15:35:38 -0700

    O(n) solution for the container w/most water problem

--- Algorithms/LeetCode/container-with-most-water.cs
+++ Algorithms/LeetCode/container-with-most-water.cs
@@ -2,40 +2,36 @@
 
 /*
  * - processing a sorted list in a certain order may allow you to prune out everything above or below a certain point.
+ * - processing a list from both left and right sides is a thing.
  * - optimizing the function without changing the run time may get you past the timeout period; don't be afraid to try.
  * - in trying to optimize the function without changing the run time, that may help you see a solution in a shorter runtime.
  */
 
 public class container_with_most_water
 {
-	public int MaxArea(int[] height)
-	{
-		int maxArea = 0;
-		for (int i = height.Length - 1; i > 0; i--)
-		{
-			int maxAreaOfI = 0;
-			int bestHeightForI = -1;
-			for (int j = 0; j < i; j++)
-			{
-				if (height[j] > bestHeightForI)
-				{
-					int candidateArea = Math.Min(height[i], height[j]) * (i - j);
-					if (candidateArea > maxAreaOfI)
-					{
-						maxAreaOfI = candidateArea;
-						bestHeightForI = height[j];
-					}
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0;
+        int left = 0;
+        int right = height.Length - 1;
+        while (left < right)
+        {
+            int candidateArea = Math.Min(height[left], height[right]) * (right - left);
+            if (candidateArea > maxArea)
+            {
+                maxArea = candidateArea;
+            }
 
-					if (height[j] >= height[i])
-					{
-						break;
-					}
-				}
-			}
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
 
-			maxArea = Math.Max(maxArea, maxAreaOfI);
-		}
-
-		return maxArea;
-	}
+        return maxArea;
+    }
 }
