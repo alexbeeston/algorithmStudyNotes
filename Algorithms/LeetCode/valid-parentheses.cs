7e58dd530784e579c9d8cfebee6a57dc74d3f5b6
Alex Beeston <abeeston@dhicorp.com>
2024-12-13 16:39:03 -0700

    valid-parens

---
+++ Algorithms/LeetCode/valid-parentheses.cs
@@ -0,0 +1,73 @@
+﻿namespace Algorithms.LeetCode;
+
+public class valid_parentheses
+{
+    private enum Chars
+    {
+        Paran,
+        Curly,
+        Square,
+    }
+
+    private Stack<Chars> stack = new Stack<Chars>();
+
+    public bool IsValid(string s)
+    {
+        foreach (char c in s)
+        {
+            if (c == '(')
+            {
+                stack.Push(Chars.Paran);
+            }
+            else if (c == ')')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                Chars top = stack.Pop();
+                if (top != Chars.Paran)
+                {
+                    return false;
+                }
+            }
+            else if (c == '[')
+            {
+                stack.Push(Chars.Square);
+            }
+            else if (c == ']')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                Chars top = stack.Pop();
+                if (top != Chars.Square)
+                {
+                    return false;
+                }
+            }
+            if (c == '{')
+            {
+                stack.Push(Chars.Curly);
+            }
+            else if (c == '}')
+            {
+                if (stack.Count == 0)
+                {
+                    return false;
+                }
+
+                Chars top = stack.Pop();
+                if (top != Chars.Curly)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return stack.Count == 0;
+    }
+}
