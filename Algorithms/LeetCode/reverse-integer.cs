e1f7352fe0329e10805323f34a19403c7fd88818
Alex Beeston <abeeston@dhicorp.com>
2024-12-17 16:41:05 -0700

    Improve run time of reverse integer to 80th percentile.

--- Algorithms/LeetCode/reverse-integer.cs
+++ Algorithms/LeetCode/reverse-integer.cs
@@ -1,79 +1,31 @@
 ﻿public class reverse_integer
 {
-	const int ten_0 = 1;
-	const int ten_1 = 10;
-	const int ten_2 = 100;
-	const int ten_3 = 1000;
-	const int ten_4 = 10000;
-	const int ten_5 = 100000;
-	const int ten_6 = 1000000;
-	const int ten_7 = 10000000;
-	const int ten_8 = 100000000;
-	const int ten_9 = 1000000000;
-
-	private static int[] powersOfTen = [ten_0, ten_1, ten_2, ten_3, ten_4, ten_5, ten_6, ten_7, ten_8, ten_9];
-
 	public int Reverse(int x)
 	{
-		bool isPositive = x >= 0;
-		if (x == int.MinValue || x == 0)
+		int reversedNumber = 0;
+		bool isPositive = x > 0;
+		while (x != 0)
 		{
-			return 0;
-		}
-		else
-		{
-			x = Math.Abs(x);
-		}
-
-		int placeValueRead = 9;
-		while (x < powersOfTen[placeValueRead]) placeValueRead--;
-
-		int number = 0;
-		int placeValueWrite = 0;
-		while (placeValueRead >= 0)
-		{
-			int digit = ExtractDigit(x, placeValueRead);
-			x -= digit * powersOfTen[placeValueRead];
-			if (placeValueWrite == 9 && digit > 2)
+			if (reversedNumber > 214748364 || reversedNumber < -214748364)
 			{
 				return 0;
 			}
 
-			int amountToAdd = (isPositive ? 1 : -1) * digit * powersOfTen[placeValueWrite];
-			if (isPositive)
+			int rightMostDigit = x % 10;
+			reversedNumber *= 10;
+			reversedNumber += rightMostDigit;
+
+			if ((isPositive && (reversedNumber < 0)) ||
+				(!isPositive && (reversedNumber > 0))
+				)
 			{
-				if (amountToAdd > int.MaxValue - number)
-				{
-					return 0;
-				}
-			}
-			else
-			{
-				if (amountToAdd < int.MinValue - number)
-				{
-					return 0;
-				}
+				return 0;
 			}
 
-			number += amountToAdd;
-			placeValueRead--;
-			placeValueWrite++;
+			x -= rightMostDigit;
+			x /= 10;
 		}
 
-		return number;
-	}
-
-	public static int ExtractDigit(int number, int placeValue) // optimize w/binary search (?)
-	{
-		for (int i = 0; i < 10; i++)
-		{
-			int testAmount = i * powersOfTen[placeValue];
-			if (number - testAmount < powersOfTen[placeValue]) // optimize by passing by ref and remembering the difference ?
-			{
-				return i;
-			}
-		}
-
-		throw new Exception("I don't think I'll get here, will debug later.");
+		return reversedNumber;
 	}
 }
