194277c99d54bb48c2cf346ae4864007f2fd3185
Alex Beeston <abeeston@dhicorp.com>
2024-12-19 23:41:52 -0700

    Close to palindromic number without reversing the entire integer

---
+++ Algorithms/LeetCode/palindrome-number.cs
@@ -0,0 +1,74 @@
+﻿namespace Algorithms.LeetCode;
+
+public class palindrome_number
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+        else if (x < 10)
+        {
+            return true;
+        }
+
+        int orderOfMagnitude = 1;
+        while ((x - Math.Pow(10, orderOfMagnitude)) >= 0)
+        {
+            orderOfMagnitude++;
+        }
+
+        orderOfMagnitude--;
+        return NumericPalindromeHelper(x, orderOfMagnitude);
+    }
+
+    /// <summary>
+    /// Assumes x > 0
+    /// </summary>
+    public bool NumericPalindromeHelper(double x, int orderOfMagnitude)
+    {
+        if (orderOfMagnitude < 0)
+        {
+            return false;
+        }
+        else if (orderOfMagnitude == 0)
+        {
+            return true;
+        }
+        else if (orderOfMagnitude == 1)
+        {
+            return x % 11 == 0;
+        }
+        else
+        {
+            double rightMostDigit = x % 10;
+            if (rightMostDigit == 0)
+            {
+                return false;
+            }
+
+            double powerOfTen = Math.Pow(10, orderOfMagnitude);
+            x -= powerOfTen * rightMostDigit;
+            if (x <= 0 || x > powerOfTen)
+            {
+                return false;
+            }
+            else
+            {
+                x -= rightMostDigit;
+                if (x >= (powerOfTen / 10))
+                {
+                    x /= 10;
+                }
+
+                if (x == 0)
+                {
+                    return true;
+                }
+
+                return NumericPalindromeHelper(x, orderOfMagnitude - 2);
+            }
+        }
+    }
+}
--- Tests/TestSandbox.cs
+++ Tests/TestSandbox.cs
@@ -8,7 +8,26 @@ public class TestSandbox
     [TestMethod]
     public void Sandbox()
     {
-        longest_palindromic_substring s = new();
-        s.LongestPalindrome("cbbd");
+        palindrome_number s = new();
+        Assert.IsTrue(s.IsPalindrome(1000110001));
+        Assert.IsTrue(s.IsPalindrome(1221));
+        Assert.IsTrue(s.IsPalindrome(2222));
+        Assert.IsTrue(s.IsPalindrome(0));
+        Assert.IsTrue(s.IsPalindrome(1));
+        Assert.IsTrue(s.IsPalindrome(909));
+        Assert.IsTrue(s.IsPalindrome(121212121));
+        Assert.IsTrue(s.IsPalindrome(4004));
+        Assert.IsTrue(s.IsPalindrome(400004));
+        Assert.IsTrue(s.IsPalindrome(4664));
+        Assert.IsTrue(s.IsPalindrome(46664));
+
+        Assert.IsFalse(s.IsPalindrome(10));
+        Assert.IsFalse(s.IsPalindrome(90000));
+        Assert.IsFalse(s.IsPalindrome(100));
+        Assert.IsFalse(s.IsPalindrome(1000000));
+        Assert.IsFalse(s.IsPalindrome(38283721));
+        Assert.IsFalse(s.IsPalindrome(45561));
+        Assert.IsFalse(s.IsPalindrome(1561));
+        Assert.IsFalse(s.IsPalindrome(9933));
     }
 }
