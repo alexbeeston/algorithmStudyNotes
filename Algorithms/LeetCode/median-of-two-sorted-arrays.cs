e895de944e04a0e2b4dfc1d12ba29c3f180791ef
Alex Beeston <abeeston@dhicorp.com>
2025-01-02 08:40:24 -0700

    not worry about median of two sorted arrays for now

---
+++ Algorithms/LeetCode/median-of-two-sorted-arrays.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+
+/* Things learned
+ * 1) Finding the middle element of an array? If odd, there will be an equal number of items to the left and right of mid. If even, there will one exta item to the left of mid.
+ */
+
+public class median_of_two_sorted_arrays
+{
+}
