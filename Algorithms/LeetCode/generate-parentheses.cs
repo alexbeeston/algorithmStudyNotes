ad55fac0b3226ed33e2d049c85e3b425095edb71
Alex Beeston <abeeston@dhicorp.com>
2025-01-02 22:47:12 -0700

    Generate Parenthesis 100%!

---
+++ Algorithms/LeetCode/generate-parentheses.cs
@@ -0,0 +1,32 @@
+﻿namespace Algorithms.LeetCode;
+
+public class Generate_parentheses
+{
+    public IList<string> GenerateParenthesis(int n)
+    {
+        List<string> result = new List<string>();
+        Helper(result, string.Empty, 0, n);
+        return result;
+    }
+
+    private void Helper(List<string> possibilities, string prefix, int openPairs, int remainingPairs)
+    {
+        if (openPairs == 0 && remainingPairs == 0)
+        {
+            possibilities.Add(prefix);
+        }
+        else if (openPairs == 0)
+        {
+            Helper(possibilities, prefix + "(", openPairs + 1, remainingPairs - 1);
+        }
+        else if (remainingPairs == 0)
+        {
+            Helper(possibilities, prefix + ")", openPairs - 1, remainingPairs);
+        }
+        else
+        {
+            Helper(possibilities, prefix + "(", openPairs + 1, remainingPairs - 1);
+            Helper(possibilities, prefix + ")", openPairs - 1, remainingPairs);
+        }
+    }
+}
