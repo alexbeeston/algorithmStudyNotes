39e9c45be62d27e2296e63ce44cd96be27163117
Alex Beeston <alexmbeeston@gmail.com>
2025-01-03 08:20:31 -0700

    WIP: longest valid parantheses

---
+++ Algorithms/LeetCode/longest-valid-parentheses.cs
@@ -0,0 +1,51 @@
+﻿namespace Algorithms.LeetCode;
+
+public class longest_valid_parentheses
+{
+	public int LongestValidParentheses(string s)
+	{
+			// ()(()
+			// ()(())
+			// ()(()))
+			// "(()(((()" stack size 4, numPops = 2, last pop @ i = 2 maxI = 7
+
+		int i = 0;
+		int maxFoundValidParentheses = 0;
+		int lastValidSize = 0;
+		while (i < s.Length)
+		{
+			int numPops = 0;
+			int stackSize = 0;
+			do
+			{
+				if (s[i] == '(')
+				{
+					stackSize++;
+				}
+				else
+				{
+					if (stackSize > 0)
+					{
+						numPops++;
+					}
+
+					stackSize--;
+				}
+
+				i++;
+			}
+			while (i < s.Length && stackSize > 0);
+
+			int currentValidSize = numPops * 2;
+			if (stackSize == 0)
+			{
+				currentValidSize += lastValidSize;
+			}
+
+			maxFoundValidParentheses = Math.Max(maxFoundValidParentheses, currentValidSize);
+			lastValidSize = currentValidSize;
+		}
+
+		return maxFoundValidParentheses;
+	}
+}
