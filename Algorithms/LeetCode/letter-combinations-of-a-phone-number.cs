a631b3b7b60376b65831a1dbd9f24d2689d76e9d
Alex Beeston <abeeston@dhicorp.com>
2025-01-06 10:26:52 -0700

    Recursion is king for problems involving identifying all possible combinations.

--- Algorithms/LeetCode/letter-combinations-of-a-phone-number.cs
+++ Algorithms/LeetCode/letter-combinations-of-a-phone-number.cs
@@ -10,61 +10,43 @@ public class letter_combinations_of_a_phone_number
 {
     public IList<string> LetterCombinations(string digits)
     {
-        if (digits.Length == 0)
-        {
-            return new List<string>();
-        }
-
-        List<string> combos = GetNewLetters(digits[0]);
-        if (digits.Length == 1)
-        {
-            return combos;
-        }
-        else
-        {
-            return Helper(combos, digits.Substring(1, digits.Length - 1));
-        }
-    }
-
-    private List<string> Helper(List<string> combinations, string digits)
-    {
-        if (digits.Length == 0)
+        List<string> combinations = new List<string>();
+        if (digits == string.Empty)
         {
             return combinations;
         }
-
-        List<string> newCombinations = new List<string>();
-        foreach (string newLetter in GetNewLetters(digits[0]))
+        else
         {
-            foreach (string oldWord in combinations)
-            {
-                newCombinations.Add(oldWord + newLetter);
-            }
+            Helper(combinations, string.Empty, digits, 0);
+            return combinations;
         }
 
-        if (digits.Length == 1)
+    }
+
+    private void Helper(List<string> combinations, string prefix, string digits, int i)
+    {
+        if (i == digits.Length)
         {
-            return newCombinations;
+            combinations.Add(prefix);
         }
         else
         {
-            return Helper(newCombinations, digits.Substring(1, digits.Length - 1));
+            foreach (char newLetter in digitLetterMapping[digits[i]])
+            {
+                Helper(combinations, prefix + newLetter, digits, i + 1);
+            }
         }
     }
 
-    private static List<string> GetNewLetters(char c)
+    private readonly Dictionary<char, char[]> digitLetterMapping = new Dictionary<char, char[]>()
     {
-        return c switch
-        {
-            '2' => ["a", "b", "c"],
-            '3' => ["d", "e", "f"],
-            '4' => ["g", "h", "i"],
-            '5' => ["j", "k", "l"],
-            '6' => ["m", "n", "o"],
-            '7' => ["p", "q", "r", "s"],
-            '8' => ["t", "u", "v"],
-            '9' => ["w", "x", "y", "z"],
-            _ => throw new ArgumentException("not a valid character"),
-        };
-    }
+        { '2', ['a', 'b', 'c'] },
+        { '3', ['d', 'e', 'f'] },
+        { '4', ['g', 'h', 'i'] },
+        { '5', ['j', 'k', 'l'] },
+        { '6', ['m', 'n', 'o'] },
+        { '7', ['p', 'q', 'r', 's'] },
+        { '8', ['t', 'u', 'v'] },
+        { '9', ['w', 'x', 'y', 'z'] },
+    };
 }
