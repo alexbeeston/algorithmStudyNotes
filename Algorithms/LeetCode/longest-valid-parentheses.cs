311cd587ee3a615af7a92378ae799eda9a2bc441
Alex Beeston <alexmbeeston@gmail.com>
2025-01-07 09:59:12 -0700

    WIP

--- Algorithms/LeetCode/longest-valid-parentheses.cs
+++ Algorithms/LeetCode/longest-valid-parentheses.cs
@@ -2,35 +2,55 @@
 
 public class longest_valid_parentheses
 {
-    public int LongestValidParentheses(string s)
-    {
-        int i = 0;
-        int longestValidParenthesis = 0;
-        Stack<int> stack = new Stack<int>();
-        int startIndexOfCurrentCandidate = 0;
-        while (i < s.Length)
-        {
-            if (s[i] == '(')
-            {
-                stack.Push(i);
-            }
-            else
-            {
-                if (stack.Count == 0)
-                {
-                    longestValidParenthesis = Math.Max(longestValidParenthesis, i - startIndexOfCurrentCandidate);
-                    startIndexOfCurrentCandidate = i + 1;
-                }
-                else
-                {
-                    stack.Pop();
-                }
-            }
+	public int LongestValidParentheses(string s)
+	{
+		int longestValidParenthesis = 0;
+		Stack<int> stack = new Stack<int>();
+		int startIndexOfCurrentCandidate = 0;
 
-            i++;
-        }
+		for (int i = 0; i < s.Length; i++)
+		{
+			if (s[i] == '(')
+			{
+				stack.Push(i);
+			}
+			else
+			{
+				if (stack.Count == 0)
+				{
+					// we didn't find a new max length
 
-        int startOfValid = stack.Count == 0 ? startIndexOfCurrentCandidate : stack.Peek() + 1;
-        return Math.Max(longestValidParenthesis, i - startOfValid);
-    }
+				}
+				else
+				{
+					stack.Pop();
+				}
+			}
+		}
+
+		while (i < s.Length)
+		{
+			if (s[i] == '(')
+			{
+				stack.Push(i);
+			}
+			else
+			{
+				if (stack.Count == 0)
+				{
+					longestValidParenthesis = Math.Max(longestValidParenthesis, i - startIndexOfCurrentCandidate);
+					startIndexOfCurrentCandidate = i + 1;
+				}
+				else
+				{
+					stack.Pop();
+				}
+			}
+
+			i++;
+		}
+
+		int startOfValid = stack.Count == 0 ? startIndexOfCurrentCandidate : stack.Peek() + 1;
+		return Math.Max(longestValidParenthesis, i - startOfValid);
+	}
 }
