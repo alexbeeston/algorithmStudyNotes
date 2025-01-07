0991b17043f0fdfe50627a6932273213a5966242
Alex Beeston <abeeston@dhicorp.com>
2025-01-07 15:34:14 -0700

    Length of last word

---
+++ Algorithms/LeetCode/length-of-last-word.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+
+public class length_of_last_word
+{
+    public int LengthOfLastWord(string s)
+    {
+        int i = s.Length - 1;
+        while (s[i] == ' ') i--;
+
+        int indexOfLastLetterInLastWord = i;
+        while (i >= 0 && s[i] != ' ') i--;
+
+        return indexOfLastLetterInLastWord - i;
+    }
+}
