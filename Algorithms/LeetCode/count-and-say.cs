4843fefe8c0d17cf58dd0e81666a66a1c46eafc7
Alex Beeston <alexmbeeston@gmail.com>
2025-01-11 22:34:30 -0700

    recursive solution to count and say

---
+++ Algorithms/LeetCode/count-and-say.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+
+public class count_and_say
+{
+	// recursive
+	public string CountAndSay(int n)
+	{
+		if (n == 1)
+		{
+			return "1";
+		}
+		else
+		{
+			string lastAnswer = CountAndSay(n - 1);
+			int i = 0;
+			int j = 0;
+			StringBuilder s = new();
+			while (i < lastAnswer.Length)
+			{
+				while (i + j < lastAnswer.Length && lastAnswer[i] == lastAnswer[i + j])
+				{
+					j++;
+				}
+
+				s.Append(j);
+				s.Append(lastAnswer[i]);
+				i += j;
+				j = 0;
+			}
+
+			return s.ToString();
+		}
+	}
+}
