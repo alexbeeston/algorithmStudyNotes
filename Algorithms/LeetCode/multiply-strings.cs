2321b6e5d0dd7111129c328318fcb61946c4213e
Alex Beeston <alexmbeeston@gmail.com>
2025-01-16 20:43:42 -0700

    placeholder for multiply strings

---
+++ Algorithms/LeetCode/multiply-strings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+public class multiply_strings
+{
+	public string Multiply(string num1, string num2)
+	{
+
+
+	}
+}
