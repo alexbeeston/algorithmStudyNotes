3b938108223287b596516b87f7ef7cea8f87b2f4
Alex Beeston <alexmbeeston@gmail.com>
2025-01-23 21:24:04 -0700

    Naive buy/sell profit

---
+++ Algorithms/LeetCode/best-time-to-buy-and-sell-stock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+
+public class best_time_to_buy_and_sell_stock
+{
+	public int MaxProfit(int[] prices)
+	{
+		int maxProfit = 0;
+		for (int i = 0; i < prices.Length - 1; i++)
+		{
+			for (int j = i + 1; j < prices.Length; j++)
+			{
+				maxProfit = Math.Max(maxProfit, prices[j] - prices[i]);
+
+			}
+		}
+
+		return maxProfit;
+	}
+}
