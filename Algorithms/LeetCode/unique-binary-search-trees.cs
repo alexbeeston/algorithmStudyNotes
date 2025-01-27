7d8455ff7490baa82187677e13537b02f87556ee
Alex Beeston <abeeston@dhicorp.com>
2025-01-26 23:27:22 -0700

    num trees dynamic programming approach.

--- Algorithms/LeetCode/unique-binary-search-trees.cs
+++ Algorithms/LeetCode/unique-binary-search-trees.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Algorithms.LeetCode;
@@ -10,6 +11,36 @@ namespace Algorithms.LeetCode;
 
 public class unique_binary_search_trees
 {
+    public int NumTrees_Dynamic(int n)
+    {
+        if (n == 1)
+        {
+            return 1;
+        }
+
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            int left = 0;
+            int right = i - 1;
+            int total = 0;
+            while (left <= right)
+            {
+                int multiplier = left == right ? 1 : 2;
+                total += dp[left] * dp[right] * multiplier;
+                left++;
+                right--;
+            }
+
+            dp[i] = total;
+        }
+
+        return dp[n];
+    }
+
     public int NumTrees_Recursive(int n)
     {
         if (n == 0 || n == 1)
