98e2c3c18fa4cec4764c3435cc2b70defe56f173
Alex Beeston <abeeston@dhicorp.com>
2025-01-31 13:25:03 -0700

    Jump Game

---
+++ Algorithms/LeetCode/jump-game.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// #55
+namespace Algorithms.LeetCode;
+
+public class jump_game
+{
+    bool CanJump(int[] nums)
+    {
+        int largestReachableIndex = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (largestReachableIndex == i && nums[i] == 0)
+            {
+                return i == nums.Length - 1;
+            }
+            else
+            {
+                largestReachableIndex = Math.Max(largestReachableIndex, i + nums[i]);
+            }
+        }
+
+        return true;
+    }
+}
