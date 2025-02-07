b8c737b1efb6d81eb4fda35af3f126a84bf0438f
Alex Beeston <abeeston@dhicorp.com>
2025-02-07 15:29:46 -0700

    Bleepin' max subarray, Kadane's algorithm

---
+++ Algorithms/LeetCode/maximum-subarray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode
+{
+    public class maximum_subarray
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            int max = nums[0];
+            int sum = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (sum < 0)
+                {
+                    sum = nums[i];
+                }
+                else
+                {
+                    sum += nums[i];
+                }
+
+                max = Math.Max(max, sum);
+            }
+
+            return max;
+        }
+    }
+}
