cc342b7785132d38cd45de1670fc63df04cafb5c
Alex Beeston <abeeston@dhicorp.com>
2025-02-07 17:02:46 -0700

    Unique Paths

---
+++ Algorithms/LeetCode/unique-paths.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.LeetCode;
+
+// #62
+public class unique_paths
+{
+    public int UniquePathsDynamic(int m, int n)
+    {
+        int[,] dpTable = new int[m, n];
+        
+        for (int i = 0; i < m; i++)
+        {
+            dpTable[i, 0] = 1;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            dpTable[0, i] = 1;
+        }
+
+        for (int i = 1; i < m; i++)
+        {
+            for (int j = 1; j < n; j++)
+            {
+                dpTable[i, j] = dpTable[i, j - 1] + dpTable[i - 1, j];
+            }
+        }
+
+        return dpTable[m - 1, n - 1];
+    }
+
+    public int UniquePathsRecursive(int m, int n)
+    {
+        int total = 0;
+        Step(m, n, 1, 1, ref total);
+        return total;
+    }
+
+    public void Step(int m, int n, int i, int j, ref int total)
+    {
+        if (i == m && j == n)
+        {
+            total++;
+        }
+
+        if (i < m)
+        {
+            Step(m, n, i + 1, j, ref total);
+        }
+
+        if (j < n)
+        {
+            Step(m, n, i, j + 1, ref total);
+        }
+    }
+}
--- Tests/TestSandbox.cs
+++ Tests/TestSandbox.cs
@@ -10,7 +10,7 @@ public class TestSandbox2
     [TestMethod]
     public void Sandbox()
     {
-        maximum_subarray s = new();
-        int a = s.MaxSubArrayDivideAndConquer([-1, 1000, 3, 2, -2, 1, -100, 4, 4]);
+        unique_paths s = new();
+        int a = s.UniquePathsRecursive(3, 2);
     }
 }
