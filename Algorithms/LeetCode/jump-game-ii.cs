6197771ed91dbb4443be779431a598194f021209
Alex Beeston <alexmbeeston@gmail.com>
2025-02-11 22:21:38 -0700

    Jump game II, only beats 15%

---
+++ Algorithms/LeetCode/jump-game-ii.cs
@@ -0,0 +1,20 @@
+﻿namespace Algorithms.LeetCode;
+
+public class jump_game_ii
+{
+	public int Jump(int[] nums)
+	{
+		int[] distances = Enumerable.Repeat(int.MaxValue, nums.Length).ToArray();
+		distances[0] = 0;
+		for (int i = 0; i < nums.Length; i++)
+		{
+			for (int j = 1; j <= nums[i] && (j + i < nums.Length); j++)
+			{
+				int index = i + j;
+				distances[index] = Math.Min(distances[index], distances[i] + 1);
+			}
+		}
+
+		return distances[nums.Length - 1];
+	}
+}
