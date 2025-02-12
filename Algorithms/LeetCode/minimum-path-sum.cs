a099c0af6cba4cefa398918c784289b60ff6dea4
Alex Beeston <abeeston@dhicorp.com>
2025-02-11 21:42:21 -0700

    minimum path

---
+++ Algorithms/LeetCode/minimum-path-sum.cs
@@ -0,0 +1,28 @@
+﻿namespace Algorithms.LeetCode;
+
+// #64
+internal class minimum_path_sum
+{
+    public int MinPathSum(int[][] grid)
+    {
+        for (int j = 1; j < grid[0].Length; j++)
+        {
+            grid[0][j] += grid[0][j - 1];
+        }
+
+        for (int i = 1; i < grid.Length; i++)
+        {
+            for (int j = 0;  j < grid[i].Length; j++)
+            {
+                if (j > 0)
+                {
+                    grid[i][j] += Math.Min(grid[i - 1][j], grid[i][j - 1]);
+                }
+                else
+                {
+                    grid[i][j] += grid[i - 1][j];
+                }
+            }
+        }
+    }
+}
