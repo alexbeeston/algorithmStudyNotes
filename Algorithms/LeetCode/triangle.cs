71bb6123267426cd526ea50f11e5c5714a1c911e
Alex Beeston <alexmbeeston@gmail.com>
2025-03-08 14:50:27 -0700

    triangle, was easy, got in about 10 minutes, start to finish.

---
+++ Algorithms/LeetCode/triangle.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.LeetCode;
+
+// #120
+public class triangle
+{
+	public int MinimumTotal(IList<IList<int>> triangle)
+	{
+		for (int i = triangle.Count - 2; i >= 0; i--)
+		{
+			for (int j = 0; j < triangle[i].Count; j++)
+			{
+				triangle[i][j] = triangle[i][j] + Math.Min(triangle[i + 1][j], triangle[i + 1][j + 1]);
+			}
+		}
+
+		return triangle[0][0];
+	}
+}
