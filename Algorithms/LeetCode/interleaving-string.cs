fb5e77b04d4c3c167e39dfe12cd786b3284461e7
Alex Beeston <alexmbeeston@gmail.com>
2025-03-08 14:25:45 -0700

    Passed interleaving

--- Algorithms/LeetCode/interleaving-string.cs
+++ Algorithms/LeetCode/interleaving-string.cs
@@ -9,37 +9,39 @@ namespace Algorithms.LeetCode;
 // #97
 public class interleaving_string
 {
-    public bool IsInterleave(string s1, string s2, string s3)
-    {
-        if (s1.Length == 0 && s2.Length == 0 && s3.Length == )
-        {
-            return true;
-        }
-        else if (s1.Length + s2.Length != s2.Length)
-        {
-            return false;
-        }
+	public bool IsInterleave(string s1, string s2, string s3)
+	{
+		if (s1.Length == 0 && s2.Length == 0 && s3.Length == 0)
+		{
+			return true;
+		}
+		else if (s1.Length + s2.Length != s3.Length)
+		{
+			return false;
+		}
 
-        bool[,] dpTable = new bool[s1.Length + 1, s2.Length + 1];
-        for (int i = 0; i < s1.Length + 1; i++)
-        {
-            for (int j = 0; j < s2.Length; j++)
-            {
-                if (i == 0 && j == 0)
-                {
-                    // out of range 
-                    dpTable[0, 1] = s1[0] == s3[0];
-                    dpTable[1, 0] = s2[0] == s3[0];
-                }
-                else if (dpTable[i,j])
-                {
-                    char charWeNeed = s3[i + j - 1];
-                    dpTable[i, j + 1] = s1[i - 1] == charWeNeed;
-                    dpTable[i + 1, j] = s2[j - 1] == charWeNeed;
-                }
-            }
-        }
+		bool[,] dpTable = new bool[s1.Length + 1, s2.Length + 1];
+		dpTable[0, 0] = true;
+		for (int i = 0; i < dpTable.GetLength(0); i++)
+		{
+			for (int j = 0; j < dpTable.GetLength(1); j++)
+			{
+				if (dpTable[i, j] && (i != dpTable.GetLength(0) - 1 || j != dpTable.GetLength(1) - 1))
+				{
+					char charWeNeed = s3[i + j];
+					if (i + 1 < dpTable.GetLength(0) && s1[i] == charWeNeed)
+					{
+						dpTable[i + 1, j] = true;
+					}
 
-        return dpTable[s1.Length, s2.Length];
-    }
+					if (j + 1 < dpTable.GetLength(1) && s2[j] == charWeNeed)
+					{
+						dpTable[i, j + 1] = true;
+					}
+				}
+			}
+		}
+
+		return dpTable[s1.Length, s2.Length];
+	}
 }
--- Tests/TestSandbox.cs
+++ Tests/TestSandbox.cs
@@ -10,8 +10,7 @@ public class TestSandbox2
     [TestMethod]
     public void Sandbox()
     {
-        maximal_rectangle s = new();
-        s.MaximalRectangle([['1', '0', '1', '0', '0'], ['1', '0', '1', '1', '1'], ['1', '1', '1', '1', '1'], ['1', '0', '0', '1', '0']]);
-        //int dog = s.MaximalRectangle([['1']]);
+        interleaving_string s = new();
+        s.IsInterleave("aabcc", "dbbca", "aadbbcbcac");
     }
 }
