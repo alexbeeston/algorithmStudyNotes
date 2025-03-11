2ae4d9eb5d1c43a49c79f0ff6c80b9c7d0d88cdc
Alex Beeston <abeeston@dhicorp.com>
2025-03-10 23:04:08 -0600

    WIP palindrome partitioning

---
+++ Algorithms/LeetCode/palindrome-partitioning.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// #131
+namespace Algorithms.LeetCode
+{
+    public class palindrome_partitioning
+    {
+        // good test case: acacbcb & eedeedfdedfd
+        public IList<IList<string>> Partition(string s)
+        {
+            bool[,] dpTable = new bool[s.Length, s.Length];
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 1; j < s.Length; j++)
+                {
+                    string substring = s.Substring(i, j - i + 1);
+                    dpTable[i, j] = substring == substring.Reverse();
+                }
+            }
+
+        }
+
+        private void Dog(string s, int startingIndex, bool[,] dpTable, List<string> currentPalindromes, List<IList<string>> allTime)
+        {
+            if (startingIndex == s.Length)
+            {
+                return;
+            }
+            if (startingIndex == s.Length - 1)
+            {
+
+            }
+
+            for (int i = startingIndex; i < s.Length; i++)
+            {
+                if (dpTable[startingIndex, i])
+                {
+                    List<string> copy = currentPalindromes.ToArray().ToList();
+                    copy.Add(s.Substring(startingIndex, i - startingIndex + 1));
+                    Dog(s, startingIndex + 1, dpTable, copy, allTime);
+                }
+            }
+        }
+    }
+}
