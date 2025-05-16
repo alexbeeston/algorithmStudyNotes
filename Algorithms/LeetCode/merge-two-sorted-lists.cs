fd7accc4d3df502e4edf00a11ebd2d22c8321ed8
Alex Beeston <abeeston@dhicorp.com>
2025-05-15 23:00:20 -0600

    merge two sorted arrays

--- Algorithms/LeetCode/merge-two-sorted-lists.cs
+++ Algorithms/LeetCode/merge-two-sorted-lists.cs
@@ -2,62 +2,50 @@
 
 public class mergeTwoLists
 {
-	public ListNode MergeTwoLists(ListNode list1, ListNode list2)
-	{
-		// init
-		ListNode head;
-		if (list1 == null && list2 == null)
-		{
-			return null;
-		}
-		else if (list1 == null)
-		{
-			head = list2;
-			list2 = list2.next;
-		}
-		else if (list2 == null)
-		{
-			head = list1;
-			list1 = list1.next;
-		}
-		else if (list1.val <= list2.val)
-		{
-			head = list1;
-			list1 = list1.next;
-		}
-		else
-		{
-			head = list2;
-			list2 = list2.next;
-		}
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        if (list1 == null && list2 == null)
+        {
+            return null;
+        }
 
-		ListNode sortedList = head;
-		while (list1 != null && list2 != null)
-		{
-			if (list1 == null)
-			{
-				sortedList.next = list2;
-				list2 = list2.next;
-			}
-			else if (list2 == null)
-			{
-				sortedList.next = list1;
-				list1 = list1.next;
-			}
-			else if (list1.val <= list2.val)
-			{
-				sortedList.next = list1;
-				list1 = list1.next;
-			}
-			else
-			{
-				sortedList.next = list2;
-				list2 = list2.next;
-			}
+        ListNode head = new ListNode();
+        Helper(head, list1, list2);
+        return head;
+    }
 
-			sortedList = sortedList.next;
-		}
+    public void Helper(ListNode tail, ListNode list1, ListNode list2)
+    {
+        if (list1 == null)
+        {
+            tail.val = list2.val;
+            list2 = list2.next;
+        }
+        else if (list2 == null)
+        {
+            tail.val = list1.val;
+            list1 = list1.next;
+        }
+        else if (list1.val <= list2.val)
+        {
+            tail.val = list1.val;
+            list1 = list1.next;
+        }
+        else
+        {
+            tail.val = list2.val;
+            list2 = list2.next;
+        }
 
-		return head;
-	}
+        if (list1 == null && list2 == null)
+        {
+            return;
+        }
+        else
+        {
+            tail.next = new ListNode();
+            tail = tail.next;
+            Helper(tail, list1, list2);
+        }
+    }
 }
