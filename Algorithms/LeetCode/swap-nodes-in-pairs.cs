44895fef9e75f6af0d6bd36790345919c861d6d5
Alex Beeston <abeeston@dhicorp.com>
2025-05-17 23:21:09 -0600

    Swap nodes in pairs, 100%

---
+++ Algorithms/LeetCode/swap-nodes-in-pairs.cs
@@ -0,0 +1,41 @@
+﻿namespace Algorithms.LeetCode;
+
+public class swap_nodes_in_pairs
+{
+    public ListNode SwapPairs(ListNode head)
+    {
+        if (head == null)
+        {
+            return null;
+        }
+        else if (head.next == null)
+        {
+            return head;
+        }
+
+        ListNode newHead = Swap(head);
+        Worker(newHead.next, newHead.next.next);
+        return newHead;
+    }
+
+    public void Worker(ListNode B, ListNode C)
+    {
+        // Given A -> B -> C, where A and B have already undergone swapping
+        if (C == null || C.next == null)
+        {
+            return;
+        }
+
+        B.next = Swap(C);
+        Worker(B.next.next, B.next.next?.next);
+    }
+
+    public ListNode Swap(ListNode A) // assumes that A.next isn't null (?)
+    {
+        // Given A -> B -> C, modify to B -> A -> C
+        ListNode B = A.next;
+        A.next = A.next.next;
+        B.next = A;
+        return B;
+    }
+}
--- Tests/TestSandbox.cs
+++ Tests/TestSandbox.cs
@@ -8,8 +8,30 @@ public class TestSandbox2
     [TestMethod]
     public void Sandbox()
     {
-        next_permutation s = new();
-        int[] testCase = [3, 2, 1];
-        s.NextPermutation(testCase);
+        swap_nodes_in_pairs s = new();
+        s.SwapPairs(new ListNode
+        {
+            val = 1,
+            next = new ListNode
+            {
+                val = 2,
+                next = new ListNode
+                {
+                    val = 3,
+                    next = new ListNode
+                    {
+                        val = 4,
+                        next = new ListNode
+                        {
+                            val = 5,
+                            //next = new ListNode
+                            //{
+                            //    val = 6,
+                            //},
+                        }
+                    }
+                }
+            }
+        });
     }
 }
