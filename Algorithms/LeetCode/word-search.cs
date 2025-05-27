b6b695abb1afe1bd3fe019fd149bdb871d6fa332
Alex Beeston <abeeston@dhicorp.com>
2025-05-26 21:34:50 -0600

    scramble string WIP

--- Algorithms/LeetCode/word-search.cs
+++ Algorithms/LeetCode/word-search.cs
@@ -4,18 +4,14 @@ public class word_search
 {
     public bool Exist(char[][] board, string word) // rewrite with consumed declared as a rectangular array.
     { 
-        bool[][] consumed = new bool[board.Length][];
-        for (int m = 0; m < board.Length; m++)
-        {
-            consumed[m] = new bool[board[m].Length];
-        }
-
+        bool[,] consumed = new bool[board.Length, board[0].Length];
+        bool[,,] dpTable = new bool[board.Length, board[0].Length, word.Length];
 
         for (int m = 0; m < board.Length; m++)
         {
             for (int n = 0; n < board[m].Length; n++)
             {
-                if (board[m][n] == word[0] && Worker(board, consumed, word, m, n, 0))
+                if (board[m][n] == word[0] && Worker(board, consumed, dpTable, word, m, n, 0))
                 {
                     return true;
                 }
@@ -25,22 +21,28 @@ public class word_search
         return false;
     }
 
-    private bool Worker(char[][] board, bool[][] consumed, string word, int m, int n, int i) // called if board[m][n] contains word[i]
+    // dpTable represent 'isDeadEnd'
+    private bool Worker(char[][] board, bool[,] consumed, bool[,,] dpTable, string word, int m, int n, int i) // called if board[m][n] is word[i]
     {
+        if (dpTable[m, n, i])
+        {
+            return false;
+        }
+
         if (i == word.Length - 1)
         {
             return true;
         }
 
-        consumed[m][n] = true;
+        consumed[m,n] = true;
 
         bool works;
         // above:
         works =
             m > 0 &&
             board[m - 1][n] == word[i + 1] &&
-            !consumed[m - 1][n] &&
-            Worker(board, consumed, word, m - 1, n, i + 1);
+            !consumed[m - 1,n] &&
+            Worker(board, consumed, dpTable, word, m - 1, n, i + 1);
         if (works)
         {
             return true;
@@ -50,8 +52,8 @@ public class word_search
         works = 
             n < board[m].Length - 1 &&
             board[m][n + 1] == word[i + 1] && 
-            !consumed[m][n + 1] && 
-            Worker(board, consumed, word, m, n + 1, i + 1);
+            !consumed[m,n + 1] && 
+            Worker(board, consumed, dpTable, word, m, n + 1, i + 1);
         if (works)
         {
             return true;
@@ -61,8 +63,8 @@ public class word_search
         works = 
             m < board.Length - 1 &&
             board[m + 1][n] == word[i + 1] && 
-            !consumed[m + 1][n] && 
-            Worker(board, consumed, word, m + 1, n, i + 1);
+            !consumed[m + 1,n] && 
+            Worker(board, consumed, dpTable, word, m + 1, n, i + 1);
         if (works)
         {
             return true;
@@ -72,14 +74,15 @@ public class word_search
         works =
             n > 0 &&
             board[m][n - 1] == word[i + 1] &&
-            !consumed[m][n - 1] &&
-            Worker(board, consumed, word, m, n - 1, i + 1);
+            !consumed[m,n - 1] &&
+            Worker(board, consumed, dpTable, word, m, n - 1, i + 1);
         if (works)
         {
             return true;
         }
 
-        consumed[m][n] = false;
+        consumed[m,n] = false;
+        dpTable[m, n, i] = true;
         return false;
     }
 }
