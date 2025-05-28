f910f95bd07278e52b0175b80d01c373eae66875
Alex Beeston <alexmbeeston@gmail.com>
2025-05-27 22:40:50 -0600

    WIP restore IP addresses

---
+++ Algorithms/LeetCode/restore-valid-ip-addresses.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms.LeetCode;
+
+public class restore_valid_ip_addresses
+{
+	public IList<string> RestoreIpAddresses(string s)
+	{
+
+
+
+	}
+
+	public void Worker(string s, int i, List<string> validIps, int octetNumber)
+	{
+		if (i == s.Length)
+		{
+
+		}
+
+		// starting at i, form an octet and then advance to next i for each octet
+	}
+}
