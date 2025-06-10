e1b708e2f8f9729f8cac48722027f54105f32757
Alex Beeston <abeeston@dhicorp.com>
2025-06-10 08:49:41 -0600

    80% on maximum-difference-between-even-and-odd-frequency-i, but 91% for memory.

---
+++ Algorithms/LeetCode/maximum-difference-between-even-and-odd-frequency-i.cs
@@ -0,0 +1,35 @@
+﻿namespace Algorithms.LeetCode;
+
+// 3442
+public class maximum_difference_between_even_and_odd_frequency_i
+{
+    public int MaxDifference(string s)
+    {
+        Dictionary<char, int> frequencies = new Dictionary<char, int>();
+        foreach (char c in s)
+        {
+            if (!frequencies.ContainsKey(c))
+            {
+                frequencies[c] = 0;
+            }
+
+            frequencies[c]++;
+        }
+
+        int maxOdd = 0;
+        int minEven = int.MaxValue;
+        foreach (char c in frequencies.Keys)
+        {
+            if (frequencies[c] % 2 == 0)
+            {
+                minEven = Math.Min(minEven, frequencies[c]);
+            }
+            else
+            {
+                maxOdd = Math.Max(maxOdd, frequencies[c]);
+            }
+        }
+
+        return maxOdd - minEven;
+    }
+}
