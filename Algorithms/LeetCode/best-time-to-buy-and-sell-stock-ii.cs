c2815e7a44fc92a2d19ba47cfe2028203dbf582b
Alex Beeston <alexmbeeston@gmail.com>
2025-06-16 14:48:17 -0600

    Best time to buy/sell II at 100% (finally). Can solve in linear time, just visualize the data next time.

--- Algorithms/LeetCode/best-time-to-buy-and-sell-stock-ii.cs
+++ Algorithms/LeetCode/best-time-to-buy-and-sell-stock-ii.cs
@@ -8,74 +8,45 @@ public class best_time_to_buy_and_sell_stock_ii
 		{
 			return 0;
 		}
-		else if (prices.Length == 2)
-		{
-			return Math.Max(0, prices[1] - prices[0]);
-		}
 		else
 		{
-			return Worker(prices, 1, 0, 0);
+			return Worker(prices, 0, 0, 0);
 		}
 	}
 
 	private int Worker(int[] prices, int i, int lastBuy, int cummulitiveProfit) // prices
 	{
-		if (i == prices.Length - 2) // on penultimate
+		bool upOrSameTomorrow = prices[i + 1] >= prices[i];
+		if (i == prices.Length - 2)
 		{
-			if (prices[i + 1] >= prices[i]) //  last day is greater than or eq penultimate
+			int lastSell = i;
+			if (upOrSameTomorrow)
 			{
 				lastBuy = lastBuy == -1 ? i : lastBuy;
-				return cummulitiveProfit + prices[i + 1] - prices[lastBuy];
+				lastSell = i + 1;
 			}
-			else
+			else if (lastBuy == -1)
 			{
-				if (lastBuy != -1)
-				{
-					cummulitiveProfit += prices[i] - prices[lastBuy];
-				}
-
-				return cummulitiveProfit;
+				lastBuy = i;
 			}
+
+			cummulitiveProfit += prices[lastSell] - prices[lastBuy];
+			return cummulitiveProfit;
 		}
 
-		bool upFromYesterday = prices[i] > prices[i - 1];
-		bool sameAsYesterday = prices[i] == prices[i - 1];
-		bool upTomorrow = prices[i + 1] > prices[i];
-		bool sameTomorrow = prices[i + 1] == prices[i];
-
-		if (upFromYesterday)
+		if (upOrSameTomorrow)
 		{
-			if (upTomorrow || sameTomorrow)
-			{
-				return Worker(prices, i + 1, lastBuy, cummulitiveProfit);
-			}
-			else
-			{
-				cummulitiveProfit += prices[i] - prices[lastBuy];
-				return Worker(prices, i + 1, -1, cummulitiveProfit);
-			}
-		}
-		else if (sameAsYesterday)
-		{
-			if (upTomorrow)
-			{
-				return Worker(prices, i + 1, i, cummulitiveProfit);
-			}
-			else
-			{
-				return Worker(prices, i + 1, -1, cummulitiveProfit);
-			}
+			lastBuy = lastBuy == -1 ? i : lastBuy;
+			return Worker(prices, i + 1, lastBuy, cummulitiveProfit);
 		}
 		else
 		{
-			if (upTomorrow)
+			if (lastBuy != -1)
 			{
-				return Worker(prices, i + 1, i, cummulitiveProfit);
+				cummulitiveProfit += prices[i] - prices[lastBuy];
 			}
-			else
-			{
-				return Worker(prices, i + 1, -1, cummulitiveProfit);
-			}
+
+			return Worker(prices, i + 1, -1, cummulitiveProfit);
 		}
 	}
 }
